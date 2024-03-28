b4a8eb8d4be812023b7422322f655b6087a818e2
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-03-28 23:01:32 +0530

    Time: 2 ms (54.04%) | Memory: 41.4 MB (45.69%) - LeetSync

--- 20-valid-parentheses/valid-parentheses.java
+++ 20-valid-parentheses/valid-parentheses.java
@@ -1,23 +1,39 @@
 class Solution {
     public boolean isValid(String s) {
         Stack<Character> stack=new Stack<>();
+        // for(int i=0;i<s.length();i++){
+        //     if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
+        //         stack.push(s.charAt(i));
+        //     }
+        //     else{
+        //         if(stack.isEmpty()){
+        //             return false;
+        //         }
+        //         else{
+        //             char ch=stack.pop();
+        //             if((s.charAt(i)==')' && ch=='(') || (s.charAt(i)==']' && ch=='[') || (s.charAt(i)=='}' && ch=='{')){
+        //                 continue;
+        //             }
+        //             else{
+        //                 return false;
+        //             }
+        //         }
+        //     }
+        // }
+        // return stack.isEmpty();
         for(int i=0;i<s.length();i++){
-            if(s.charAt(i)=='(' || s.charAt(i)=='{' || s.charAt(i)=='['){
-                stack.push(s.charAt(i));
+            char ch=s.charAt(i);
+            if(ch=='('){
+                stack.push(')');
             }
-            else{
-                if(stack.isEmpty()){
-                    return false;
-                }
-                else{
-                    char ch=stack.pop();
-                    if((s.charAt(i)==')' && ch=='(') || (s.charAt(i)==']' && ch=='[') || (s.charAt(i)=='}' && ch=='{')){
-                        continue;
-                    }
-                    else{
-                        return false;
-                    }
-                }
+            else if(ch=='{'){
+                stack.push('}');
+            }
+            else if(ch=='['){
+                stack.push(']');
+            }
+            else if(stack.isEmpty() || stack.pop()!=ch){
+                return false;
             }
         }
         return stack.isEmpty();
