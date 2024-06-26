f849a49b40569464607c7715c95f8b6a34e1b5e4
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-06-26 22:15:44 +0530

    Time: 0 ms (100.00%) | Memory: 42.8 MB (37.78%) - LeetSync

--- 35-search-insert-position/search-insert-position.java
+++ 35-search-insert-position/search-insert-position.java
@@ -7,9 +7,10 @@ class Solution {
             if(nums[mid]==target){
                 return mid;
             }
-            if(nums[mid]<target){
+            else if(nums[mid]<target){
                 left=mid+1;
-            }else{
+            }
+            else{
                 right=mid-1;
             }
         }
