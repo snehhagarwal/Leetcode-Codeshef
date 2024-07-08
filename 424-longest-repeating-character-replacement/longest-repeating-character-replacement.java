26e7f4d6df16490e2d162c209c952ec2c8360adf
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-07-08 12:18:08 +0530

    Time: 11 ms (48.70%) | Memory: 42.5 MB (47.98%) - LeetSync

--- 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
+++ 424-longest-repeating-character-replacement/longest-repeating-character-replacement.java
@@ -1,20 +1,38 @@
 class Solution {
     public int characterReplacement(String s, int k) {
         //brute force
+        // int max=0;
+        // for(int i=0;i<s.length();i++){
+        //     int[] arr=new int[26];
+        //     int maxfreq=0;
+        //     for(int j=i;j<s.length();j++){
+        //         arr[s.charAt(j)-'A']++;
+        //         maxfreq=Math.max(maxfreq,arr[s.charAt(j)-'A']);
+        //         int changes=(j-i+1)-maxfreq;
+        //         if(changes<=k){
+        //             max=Math.max(j-i+1,max);
+        //         }else{
+        //             break;
+        //         }
+        //     }
+        // }
+        // return max;
+        //better solution
+        int i=0;
+        int j=0;
         int max=0;
-        for(int i=0;i<s.length();i++){
-            int[] arr=new int[26];
-            int maxfreq=0;
-            for(int j=i;j<s.length();j++){
-                arr[s.charAt(j)-'A']++;
-                maxfreq=Math.max(maxfreq,arr[s.charAt(j)-'A']);
-                int changes=(j-i+1)-maxfreq;
-                if(changes<=k){
-                    max=Math.max(j-i+1,max);
-                }else{
-                    break;
-                }
+        int maxfreq=0;
+        int[] arr= new int[26];
+        while(j<s.length()){
+            arr[s.charAt(j)-'A']++;
+            maxfreq=Math.max(maxfreq,arr[s.charAt(j)-'A']);
+            //int changes=(j-i+1)-maxfreq;
+            if((j-i+1)-maxfreq>k){
+                arr[s.charAt(i)-'A']--;
+                i++;
             }
+            max=Math.max(j-i+1,max);
+            j++;
         }
         return max;
     }
