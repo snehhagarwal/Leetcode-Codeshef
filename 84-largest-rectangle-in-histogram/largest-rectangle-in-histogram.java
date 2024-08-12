2d5af3cc4d601415ff7e0893f4f9d7577d02888d
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-08-13 00:34:17 +0530

    Time: 65 ms (67.46%) | Memory: 57.7 MB (27.30%) - LeetSync

--- 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
+++ 84-largest-rectangle-in-histogram/largest-rectangle-in-histogram.java
@@ -1,46 +1,77 @@
 class Solution {
     public int largestRectangleArea(int[] heights) {
-        int[] nse=nsefunc(heights);
-        int[] pse=psefunc(heights);
+        Stack<Integer> st=new Stack<>();
         int max=0;
+        int nse=0;
+        int pse=0;
         for(int i=0;i<heights.length;i++){
-            int area=heights[i]*(nse[i]-pse[i]-1);
-            max=Math.max(max,area);
+            while(!st.isEmpty() && heights[st.peek()]>heights[i]){
+                int ele=st.pop();
+                nse=i;
+                if(st.isEmpty()){
+                    pse=-1;
+                }else{
+                    pse=st.peek();
+                }
+                int area=heights[ele]*(nse-pse-1);
+                max=Math.max(max,area);
+            }
+            st.push(i);
+        }
+        while(!st.isEmpty()){
+            nse=heights.length;
+            int ele=st.pop();
+            if(st.isEmpty()){
+                pse=-1;
+            }else{
+                pse=st.peek();
+            }
+            int area=heights[ele]*(nse-pse-1);
+            max=Math.max(area,max);
         }
         return max;
-     }
-     public static int[] nsefunc(int[] arr){
-        int[] nse=new int[arr.length];
-        Stack<Integer> st=new Stack<>();
-        for(int i=arr.length-1;i>=0;i--){
-            while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                nse[i]=arr.length;
-            }
-            else{
-                nse[i]=st.peek();
-            }
-            st.push(i);
-        }
-        return nse;
-    }
-    public static int[] psefunc(int[] arr){
-        int[] pse=new int[arr.length];
-        Stack<Integer> st=new Stack<>();
-        for(int i=0;i<arr.length;i++){
-            while(!st.isEmpty() && arr[st.peek()]>arr[i]){
-                st.pop();
-            }
-            if(st.isEmpty()){
-                pse[i]=-1;
-            }
-            else{
-                pse[i]=st.peek();
-            }
-            st.push(i);
-        }
-        return pse;
     }
 }
+// int[] nse=nsefunc(heights);
+//         int[] pse=psefunc(heights);
+//         int max=0;
+//         for(int i=0;i<heights.length;i++){
+//             int area=heights[i]*(nse[i]-pse[i]-1);
+//             max=Math.max(max,area);
+//         }
+//         return max;
+//      }
+//      public static int[] nsefunc(int[] arr){
+//         int[] nse=new int[arr.length];
+//         Stack<Integer> st=new Stack<>();
+//         for(int i=arr.length-1;i>=0;i--){
+//             while(!st.isEmpty() && arr[st.peek()]>=arr[i]){
+//                 st.pop();
+//             }
+//             if(st.isEmpty()){
+//                 nse[i]=arr.length;
+//             }
+//             else{
+//                 nse[i]=st.peek();
+//             }
+//             st.push(i);
+//         }
+//         return nse;
+//     }
+//     public static int[] psefunc(int[] arr){
+//         int[] pse=new int[arr.length];
+//         Stack<Integer> st=new Stack<>();
+//         for(int i=0;i<arr.length;i++){
+//             while(!st.isEmpty() && arr[st.peek()]>arr[i]){
+//                 st.pop();
+//             }
+//             if(st.isEmpty()){
+//                 pse[i]=-1;
+//             }
+//             else{
+//                 pse[i]=st.peek();
+//             }
+//             st.push(i);
+//         }
+//         return pse;
+//     }
