9e058e02117dcc9f0768c08145c380c0e68e591a
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-09-17 10:54:32 +0530

    Time: 1 ms (100.00%) | Memory: 43.8 MB (99.10%) - LeetSync

--- 23-merge-k-sorted-lists/merge-k-sorted-lists.java
+++ 23-merge-k-sorted-lists/merge-k-sorted-lists.java
@@ -1,42 +1,54 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
 class Solution {
-        public ListNode mergeKLists(ListNode[] lists) {
-                if (lists == null || lists.length == 0) {
-                            return null;
-                                    }
-                                            return mergeKListsHelper(lists, 0, lists.length - 1);
-                                                }
-                                                    
-                                                        private ListNode mergeKListsHelper(ListNode[] lists, int start, int end) {
-                                                                if (start == end) {
-                                                                            return lists[start];
-                                                                                    }
-                                                                                            if (start + 1 == end) {
-                                                                                                        return merge(lists[start], lists[end]);
-                                                                                                                }
-                                                                                                                        int mid = start + (end - start) / 2;
-                                                                                                                                ListNode left = mergeKListsHelper(lists, start, mid);
-                                                                                                                                        ListNode right = mergeKListsHelper(lists, mid + 1, end);
-                                                                                                                                                return merge(left, right);
-                                                                                                                                                    }
-                                                                                                                                                        
-                                                                                                                                                            private ListNode merge(ListNode l1, ListNode l2) {
-                                                                                                                                                                    ListNode dummy = new ListNode(0);
-                                                                                                                                                                            ListNode curr = dummy;
-                                                                                                                                                                                    
-                                                                                                                                                                                            while (l1 != null && l2 != null) {
-                                                                                                                                                                                                        if (l1.val < l2.val) {
-                                                                                                                                                                                                                        curr.next = l1;
-                                                                                                                                                                                                                                        l1 = l1.next;
-                                                                                                                                                                                                                                                    } else {
-                                                                                                                                                                                                                                                                    curr.next = l2;
-                                                                                                                                                                                                                                                                                    l2 = l2.next;
-                                                                                                                                                                                                                                                                                                }
-                                                                                                                                                                                                                                                                                                            curr = curr.next;
-                                                                                                                                                                                                                                                                                                                    }
-                                                                                                                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                                                                                                                    curr.next = (l1 != null) ? l1 : l2;
-                                                                                                                                                                                                                                                                                                                                            
-                                                                                                                                                                                                                                                                                                                                                    return dummy.next;
-                                                                                                                                                                                                                                                                                                                                                        }
-                                                                                                                                                                                                                                                                                                                                                        }
-
+    public ListNode mergeKLists(ListNode[] lists) {
+        if(lists==null || lists.length==0){
+            return null;
+        }
+        return merge(lists,0,lists.length-1);
+    }
+    public static ListNode merge(ListNode[] lists,int start,int end){
+        if(start==end){
+            return lists[start];
+        }
+        int mid=start+(end-start)/2;
+        ListNode left=merge(lists,start,mid);
+        ListNode right=merge(lists,mid+1,end);
+        return merger(left,right);
+    }
+    public static ListNode merger(ListNode left,ListNode right){
+        ListNode dummy=new ListNode(-1);
+        ListNode temp=dummy;
+        while(left!=null && right!=null){
+            if(left.val<=right.val){
+                temp.next=left;
+                left=left.next;
+                temp=temp.next;
+            }
+            else{
+                temp.next=right;
+                right=right.next;
+                temp =temp.next;
+            }
+        }
+        while(left!=null){
+            temp.next=left;
+            left=left.next;
+            temp=temp.next;
+        }
+        while(right!=null){
+            temp.next=right;
+            right=right.next;
+            temp=temp.next;
+        }
+        return dummy.next;
+    }
+}
