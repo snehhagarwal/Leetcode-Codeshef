b616af5aba4da867709c8957db54a64ba6f8dfc0
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-10-14 08:49:04 +0530

    Time: 6 ms (83.98%) | Memory: 45.2 MB (50.36%) - LeetSync

--- 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
+++ 387-first-unique-character-in-a-string/first-unique-character-in-a-string.java
@@ -1,27 +1,27 @@
 class Solution {
     public int firstUniqChar(String s) {
-        int result=Integer.MAX_VALUE;
+        // int result=Integer.MAX_VALUE;
+        // for(int i=0;i<s.length();i++){
+        //     char ch=s.charAt(i);
+        //     int st=s.indexOf(ch);
+        //     int en=s.lastIndexOf(ch);
+        //     if(st==en){
+        //         result=Math.min(st,result);
+        //     }
+        // }
+        // if(result!=Integer.MAX_VALUE){
+        //     return result;
+        // }
+        // return -1;
+        int[] arr=new int[26];
         for(int i=0;i<s.length();i++){
-            char ch=s.charAt(i);
-            int st=s.indexOf(ch);
-            int en=s.lastIndexOf(ch);
-            if(st==en){
-                result=Math.min(st,result);
-            }
+            arr[s.charAt(i)-'a']++;
         }
-        if(result!=Integer.MAX_VALUE){
-            return result;
+        for(int i=0;i<s.length();i++){
+            if(arr[s.charAt(i)-'a']==1){
+                return i;
+            }
         }
         return -1;
     }
 }
-// int[] arr=new int[26];
-        // for(int i=0;i<s.length();i++){
-        //     arr[s.charAt(i)-'a']++;
-        // }
-        // for(int i=0;i<arr.length;i++){
-        //     if(arr[s.charAt(i)-'a']==1){
-        //         return i;
-        //     }
-        // }
-        // return -1;
