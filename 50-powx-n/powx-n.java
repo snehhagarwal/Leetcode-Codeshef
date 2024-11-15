8a5e4fdd96c941501db03ee3b084369a93a37fc2
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2024-11-15 23:10:36 +0530

    Time: 0 ms (100.00%) | Memory: 42.2 MB (45.57%) - LeetSync

---
+++ 50-powx-n/powx-n.java
@@ -0,0 +1,19 @@
+class Solution {
+    public double myPow(double x, int n) {
+        if(n==0){
+            return 1;
+        }
+        long N=n;
+        if(N<0){
+            x=1/x;
+            N=-N;  
+        }
+        if(N%2==0){
+            return myPow(x*x,(int)(N/2));
+        }
+        else{
+            return x*myPow(x,(int)(N-1));
+        }
+    }
+
+}
