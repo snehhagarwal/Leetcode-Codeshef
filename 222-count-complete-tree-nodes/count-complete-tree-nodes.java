da9b8a62a1bb4d19810b77fd78dde0aab1386553
Sneha Agarwal <124284258+snehhagarwal@users.noreply.github.com>
2025-03-28 02:17:11 +0530

    Time: 0 ms (100.00%) | Memory: 47.5 MB (85.69%) - LeetSync

--- 222-count-complete-tree-nodes/count-complete-tree-nodes.java
+++ 222-count-complete-tree-nodes/count-complete-tree-nodes.java
@@ -13,13 +13,44 @@
  *     }
  * }
  */
+
+//formula we usually use is 2^height -1 
+//height same of left and right then use formula else add 1 in it and then 
 class Solution {
     public int countNodes(TreeNode root) {
+        // if(root==null){
+        //     return 0;
+        // }
+        // int left=countNodes(root.left);
+        // int right=countNodes(root.right);
+        // return left+right+1;
+
         if(root==null){
             return 0;
         }
-        int left=countNodes(root.left);
-        int right=countNodes(root.right);
-        return left+right+1;
+        int left=hf(root);
+        int right=hr(root);
+        if(left==right){
+            return (int)Math.pow(2,left)-1;
+        }
+        else return countNodes(root.left)+countNodes(root.right)+1;
+    }
+
+    public int hf(TreeNode root){
+        int cnt=1;
+        while(root.left!=null){
+            root=root.left;
+            cnt++;
+        }
+        return cnt;
+    }
+
+    public int hr(TreeNode root){
+        int cnt=1;
+        while(root.right!=null){
+            root=root.right;
+            cnt++;
+        }
+        return cnt;
     }
 }
